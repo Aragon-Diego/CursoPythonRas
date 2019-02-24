7cd3f2ec0ff388d427cb91041df72eb6112e37ea
Diego Aragon <diego.aragonCursos@gmail.com>
2019-02-24 16:34:43 -0600

    sin prueba y operaciones

---
+++ operadores.py
@@ -0,0 +1,9 @@
+x=10
+y=12
+
+print("suma:"+str(x+y))
+print("resta:"+str(x-y))
+print("multiplicacion:"+str(x*y))
+print("division flotante:"+str(x/y))
+print("division de enteros:"+str(x//y))
+print("Mod:"+str(x%y))
